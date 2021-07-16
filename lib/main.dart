a9cdf219a2220179d53521e1a40f3a1f333b4009
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2021-07-16 12:02:31 +0530

    Adding a TabBar to the AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
       // home: CategoriesScreen(),
       initialRoute: '/', // default is '/'
       routes: {
-        '/': (ctx) => CategoriesScreen(), // same as using home route
+        '/': (ctx) => TabsScreen(), // same as using home route
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
